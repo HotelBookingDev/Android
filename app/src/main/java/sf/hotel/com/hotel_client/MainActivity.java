418598887d71de745a2d9b34a6d721f31b4b5f71
林其望 <lqw@dingphone.com>
2016-06-03 14:47:06 +0800

    配置

--- app/src/main/java/sf/hotel/com/hotel_client/MainActivity.java
+++ app/src/main/java/sf/hotel/com/hotel_client/MainActivity.java
@@ -2,8 +2,6 @@ package sf.hotel.com.hotel_client;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
-import android.view.View;
-import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
