1487108c5021e31d967951f0fd159a96ee793cc0
林其望 <lqw@dingphone.com>
2016-06-03 13:57:24 +0800

    gradle 简单配置

--- app/src/main/java/sf/hotel/com/hotel_client/MainActivity.java
+++ app/src/main/java/sf/hotel/com/hotel_client/MainActivity.java
@@ -1,7 +1,7 @@
 package sf.hotel.com.hotel_client;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
 
 public class MainActivity extends AppCompatActivity {
 
---
+++ app/src/main/java/sf/hotel/com/hotel_client/Utils/TToast.java
@@ -0,0 +1,15 @@
+package sf.hotel.com.hotel_client.Utils;
+
+/**
+ * Created by FMT on 2016/6/3:12:28
+ * EMAILE 1105896230@qq.com.
+ */
+public final class TToast {
+    private TToast() {
+        throw new RuntimeException("can not have examples");
+    }
+
+    public void showToast(String message) {
+
+    }
+}
