7d1af7aad4e664c650fac6fc6cbea75466d701a3
sanfen <sanfen@sanfendeMacBook-Pro.local>
2016-06-06 12:04:33 +0800

    集成bugly

--- app/src/main/java/sf/hotel/com/hotel_client/HotelApplication.java
+++ app/src/main/java/sf/hotel/com/hotel_client/HotelApplication.java
@@ -3,6 +3,8 @@ package sf.hotel.com.hotel_client;
 import android.app.Application;
 import android.content.Context;
 
+import com.tencent.bugly.crashreport.CrashReport;
+
 /**
  * Created by FMT on 2016/6/3:19:04
  * EMAILE 1105896230@qq.com.
@@ -10,9 +12,20 @@ import android.content.Context;
 public class HotelApplication extends Application {
     public static Context context;
 
+
     @Override
     public void onCreate() {
         super.onCreate();
         context = this.getApplicationContext();
+
+        //初始化bugly
+        initBuglyStatus();
+    }
+
+
+    public void initBuglyStatus(){
+        CrashReport.UserStrategy strategy = new CrashReport.UserStrategy(context);
+        strategy.setAppVersion("1.0.1");
+        CrashReport.initCrashReport(context, "900033362", true, strategy);
     }
 }
