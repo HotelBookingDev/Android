0db44251fe06222c81f26f7bad424a7eefbacc88
lqw <1105896230@qq.com>
2016-06-16 10:56:44 +0800

    修改代码

--- app/src/main/java/sf/hotel/com/hotel_client/view/fragment/person/SettingFragment.java
+++ app/src/main/java/sf/hotel/com/hotel_client/view/fragment/person/SettingFragment.java
@@ -7,14 +7,28 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
+import java.io.File;
+
+import butterknife.BindView;
 import butterknife.ButterKnife;
 import butterknife.OnClick;
+import mehdi.sakout.fancybuttons.FancyButton;
+import rx.android.schedulers.AndroidSchedulers;
+import rx.functions.Action1;
+import rx.functions.Func1;
+import rx.schedulers.Schedulers;
+import sf.hotel.com.data.config.HotelConstant;
 import sf.hotel.com.data.utils.HotelFileUtils;
+import sf.hotel.com.data.utils.StringUtils;
+import sf.hotel.com.hotel_client.HotelApplication;
 import sf.hotel.com.hotel_client.R;
 import sf.hotel.com.hotel_client.view.activity.LoginActivity;
 import sf.hotel.com.hotel_client.view.fragment.BaseFragment;
 
 public class SettingFragment extends BaseFragment {
+
+    @BindView(R.id.setting_clear)
+    FancyButton mClearBtn;
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
@@ -22,17 +36,34 @@ public class SettingFragment extends BaseFragment {
         // Inflate the layout for this fragment
         View view = inflater.inflate(R.layout.fragment_setting, container, false);
         ButterKnife.bind(this, view);
+        initView();
         return view;
     }
 
+    private void initView() {
+        setBtnClearText();
+    }
+
     @OnClick(R.id.setting_out)
-    public void logingOut(){
-        Intent intent=new Intent(getActivity(), LoginActivity.class);
+    public void logingOut() {
+        Intent intent = new Intent(getActivity(), LoginActivity.class);
         intent.setFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
         startActivity(intent);
     }
+
     @OnClick(R.id.setting_clear)
-    public void settingclear(){
+    public void settingclear() {
         HotelFileUtils.clearImageDir();
+        setBtnClearText();
+    }
+
+    private void setBtnClearText() {
+        rx.Observable.just(mClearBtn).subscribeOn(Schedulers.io())
+                .unsubscribeOn(Schedulers.io())
+                .observeOn(AndroidSchedulers.mainThread()).map(fancyButton -> HotelFileUtils
+                .getSize(new File(HotelConstant.TEMP_IMG_DIR))).filter(s -> s != null).subscribe
+                (s -> {
+                    mClearBtn.setText(mClearBtn.getText() + "(" + s + ")");
+                });
     }
 }
--- data/src/main/java/sf/hotel/com/data/utils/HotelFileUtils.java
+++ data/src/main/java/sf/hotel/com/data/utils/HotelFileUtils.java
@@ -27,4 +27,29 @@ public class HotelFileUtils {
         }
         return  isExit;
     }
+
+    public static String getSize(File file){
+        String size=null;
+        if (isExit(file)){
+            size=convertFileSize(file.length());
+        }
+        return size;
+    }
+
+    public static String convertFileSize(long size) {
+        long kb = 1024;
+        long mb = kb * 1024;
+        long gb = mb * 1024;
+
+        if (size >= gb) {
+            return String.format("%.1f GB", (float) size / gb);
+        } else if (size >= mb) {
+            float f = (float) size / mb;
+            return String.format(f > 100 ? "%.0f MB" : "%.1f MB", f);
+        } else if (size >= kb) {
+            float f = (float) size / kb;
+            return String.format(f > 100 ? "%.0f KB" : "%.1f KB", f);
+        } else
+            return null;
+    }
 }
