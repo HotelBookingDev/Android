218330f8614151275e1015d591550dccaeffc468
林其望 <1105896230@qq.com>
2016-06-30 18:48:51 +0800

    删除资源文件

--- app/src/main/java/sf/hotel/com/hotel_client/view/fragment/person/EvauleFragment.java
+++ app/src/main/java/sf/hotel/com/hotel_client/view/fragment/person/EvauleFragment.java
@@ -27,7 +27,7 @@ public class EvauleFragment extends BaseFragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
             Bundle savedInstanceState) {
-        View view = inflater.inflate(R.layout.fragment_evaule, container, false);
+        View view = inflater.inflate(R.layout.fragment_ evaule, container, false);
         ButterKnife.bind(this, view);
         initViews();
         return view;
