a815418967077447178bfc439fdfcaf5a090d827
林其望 <1105896230@qq.com>
2016-06-30 17:33:03 +0800

    修改界面

--- app/src/main/java/sf/hotel/com/hotel_client/view/fragment/person/PersonFragment.java
+++ app/src/main/java/sf/hotel/com/hotel_client/view/fragment/person/PersonFragment.java
@@ -9,9 +9,8 @@ import android.view.ViewGroup;
 import butterknife.ButterKnife;
 import butterknife.OnClick;
 import rx.Subscription;
-import sf.hotel.com.data.utils.WebViewModelFactory;
 import sf.hotel.com.hotel_client.R;
-import sf.hotel.com.hotel_client.view.activity.WebViewActivity;
+import sf.hotel.com.hotel_client.view.activity.person.EvalueActivity;
 import sf.hotel.com.hotel_client.view.activity.person.MoneyActivity;
 import sf.hotel.com.hotel_client.view.activity.person.OrderActivity;
 import sf.hotel.com.hotel_client.view.activity.person.SettingActivity;
@@ -91,9 +90,7 @@ public class PersonFragment extends BaseFragment implements IPersonView {
             Intent intent = new Intent(getActivity(), MoneyActivity.class);
             startActivity(intent);
         } else if (type == EVALUATE) {
-            Intent intent = new Intent(getActivity(), WebViewActivity.class);
-            intent.putExtra(WebViewActivity.KEY,
-                    (WebViewModelFactory.getModel(WebViewModelFactory.BAIDU)));
+            Intent intent = new Intent(getActivity(), EvalueActivity.class);
             startActivity(intent);
         }
     }
